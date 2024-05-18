ad11fa6b37879eb45373c514b28ea5f71468a52c
sverben <59171289+sverben@users.noreply.github.com>
2024-05-18 16:28:04 +0200

    chore: switch to svelte

--- scripts/build-leaphy-webbased.js
+++ scripts/build-leaphy-webbased.js
@@ -5,15 +5,18 @@ if (!fs.existsSync("deps")) fs.mkdirSync("deps");
 
 async function buildWebbased() {
   process.chdir("deps");
-  execSync("git clone https://github.com/leaphy-robotics/leaphy-webbased");
-  process.chdir("leaphy-webbased");
+  execSync("git clone https://github.com/leaphy-robotics/leaphy-webbased-svelte");
+  process.chdir("leaphy-webbased-svelte");
 
   execSync("yarn install");
-  execSync("yarn buildLocal");
 
-  fs.renameSync("dist/browser", "../public");
+  process.env.VITE_BACKEND_URL = "http://localhost:8000"
+  process.env.NODE_OPTIONS = "--max-old-space-size=4096"
+  execSync("yarn build");
+
+  fs.renameSync("dist", "../public");
   process.chdir("..");
-  fs.rmSync("leaphy-webbased", { recursive: true, force: true });
+  fs.rmSync("leaphy-webbased-svelte", { recursive: true, force: true });
 
   console.log("Webbased build complete");
 }
